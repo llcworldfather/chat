43f7784f3d0aca5c92417f2c5b5afedbe3759b47
梁立成 <1599028247@qq.com>
2025-11-20 10:01:15 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChatProvider } from './context/ChatContext';
+import { AuthContainer } from './components/Auth/AuthContainer';
+import { ChatContainer } from './components/Chat/ChatContainer';
+import { useChat } from './context/ChatContext';
+
+function AppContent() {
+  const { user } = useChat();
+
+  console.log('AppContent rendering, user:', user);
+
+  return user ? <ChatContainer /> : <AuthContainer />;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatProvider>
+      <AppContent />
+    </ChatProvider>
   );
 }
 
